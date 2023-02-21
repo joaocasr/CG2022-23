d2719df2af689a54d6f14dfc2926bbc57756b79c
joaocasr <castrojoao839@gmail.com>
2023-02-21 22:49:47 +0000

    tinyxml read xml-fase1

---
+++ FASE1/main.h
@@ -0,0 +1,16 @@
+#include<vector>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void parse_XML();
